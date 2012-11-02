669860ee13b58c2128f11cdc4c85a24e76d1d080
Victor Castell <victorcoder@gmail.com>
2012-11-02 14:35:56 +0100

    Redis install from source

--- ruby/manifests/init.pp
+++ ruby/manifests/init.pp
@@ -1,20 +1,32 @@
 class ruby($version = 'ruby-1.9.3-p125') {
-  include stow
+  include "ruby::$operatingsystemrelease"
+  
+  class 10.04 {
+    include stow
 
-  file { "ruby-stow-boot":
-    ensure => 'present',
-    path => '/root/install-ruby-stow',
-    owner => 'root', group => 'root', mode => '0774',
-    source => 'puppet:///modules/ruby/install-ruby-stow';
+    file { "ruby-stow-boot":
+      ensure => 'present',
+      path => '/root/install-ruby-stow',
+      owner => 'root', group => 'root', mode => '0774',
+      source => 'puppet:///modules/ruby/install-ruby-stow';
+    }
+
+    exec { "ruby-stow":
+      command => "/root/install-ruby-stow",
+      creates => "/usr/local/stow/${version}",
+      timeout => "-1",
+      require => [Package["stow"], File["ruby-stow-boot"]];
+    }
+
   }
 
-  exec { "ruby-stow":
-    command => "/root/install-ruby-stow",
-    creates => "/usr/local/stow/${version}",
-    timeout => "-1",
-    require => [Package["stow"], File["ruby-stow-boot"]];
+  class 12.04 {
+    $packagelist = ['ruby1.9.1', 'ruby1.9.1-dev', 'libruby1.9.1']
+    package { $packagelist: 
+      ensure => installed
+    }
   }
-
+  
   # Dependencies
   $packagelist = ['sqlite3', 'libsqlite3-dev', 'libreadline-dev', 'libxml2', 'libxml2-dev', 'libxslt1-dev', 'build-essential', 'libcurl4-openssl-dev']
   package { $packagelist: 
