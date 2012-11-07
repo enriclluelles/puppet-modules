9d269d32bb12f529b035a7f657aebbfae9101063
Francesc Esplugas <francesc.esplugas@gmail.com>
2012-11-07 11:32:40 +0100

    Fixed apt::ppa

--- apt/manifests/ppa.pp
+++ apt/manifests/ppa.pp
@@ -1,11 +1,16 @@
 # $name is arbitrary. attribute "ppa" should be the ppa name, such as
 # "brightbox/ruby-ng" (this is due to a bug in puppet with resources
 # with slashes in them)
-define apt::ppa($ppa = "") {
+define apt::ppa() {
 
-  exec { "apt-add-repository-${ppa}":
-    command => "/usr/bin/apt-add-repository ppa:${ppa}",
-    creates => ppa_filename($ppa),
+  case $::operatingsystemrelease {
+    '12.04': { $cmd = '/usr/bin/add-apt-repository --yes' }
+    default: { $cmd = '/usr/bin/add-apt-repository' }
+  }
+
+  exec { "apt-add-repository-${name}":
+    command => "${cmd} ppa:${name}",
+    creates => ppa_filename($name),
     notify => Exec["apt-update"],
   }
 
