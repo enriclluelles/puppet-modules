2fcc00ef5a68d1910ed686ef8842cedc4ad3b2af
Francesc Esplugas <francesc.esplugas@gmail.com>
2012-11-07 10:50:15 +0100

    Lint and cleanup

--- imagemagick/manifests/init.pp
+++ imagemagick/manifests/init.pp
@@ -1,19 +1,11 @@
-#
-# Class: imagemagick
-#
-# Include it to install and imagemagick
-# It defines package.
-#
-# Usage:
-# include imagemagick
-#
-
 class imagemagick {
-  package{ "imagemagick":
+
+  package { 'imagemagick':
     ensure => installed,
   }
-  
-  package{ "libmagickwand-dev":
+
+  package { 'libmagickwand-dev':
     ensure => installed,
   }
+
 }
