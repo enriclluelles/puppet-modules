24ccf468cf23dd9a06946ecbfafbef436d56c037
Francesc Esplugas <francesc.esplugas@gmail.com>
2012-11-08 12:56:59 +0100

    No longer

--- base/manifests/init.pp
+++ base/manifests/init.pp
@@ -6,15 +6,4 @@ class base {
   include base::time
   include base::packages
 
-  host { "${hostname}.lan" :
-    ensure => present,
-    host_aliases => $hostname,
-    ip => "127.0.0.1"
-  }
-
-  host { 'localhost' :
-    ensure => present,
-    ip => "127.0.0.1"
-  }
-
 }
