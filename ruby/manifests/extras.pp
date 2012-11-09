c0e414a2caf9c9b455f4daa7269cbe53465e55a3
Francesc Esplugas <francesc.esplugas@gmail.com>
2012-11-09 14:30:32 +0100

    Configurable

--- ruby/manifests/extras.pp
+++ ruby/manifests/extras.pp
@@ -1,11 +1,13 @@
-class ruby::extras {
+class ruby::extras(
+  $ensure = 'present',
+) {
 
   if ! defined(Package['libxslt-dev']) {
-    package { 'libxslt-dev': ensure => latest; }
+    package { 'libxslt-dev': ensure => $ensure; }
   }
 
   if ! defined(Package['libxml2-dev']) {
-    package { 'libxml2-dev': ensure => latest; }
+    package { 'libxml2-dev': ensure => $ensure; }
   }
 
 }
