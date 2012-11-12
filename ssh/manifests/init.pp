95714e0ee7480c06c4684efdcb477fe5b1a822e2
Francesc Esplugas <francesc.esplugas@gmail.com>
2012-11-12 12:07:02 +0100

    Gardening

--- ssh/manifests/init.pp
+++ ssh/manifests/init.pp
@@ -1,14 +1,23 @@
-# Manage a bit of ssh properties
 class ssh {
-	define append_ssh_key_to_root($key) {
-    append_ssh_key_to_user {$name:
-      user => "root", 
+
+  define append_ssh_key_to_root(
+    $key,
+  ) {
+
+    append_ssh_key_to_user { $name:
+      user => root,
       key => $key,
     }
+
   }
 
-  define append_ssh_key_to_user($user, $key, $key_type="ssh-rsa") {
-    ssh_authorized_key {$name:
+  define append_ssh_key_to_user(
+    $user,
+    $key,
+    $key_type = 'ssh-rsa',
+  ) {
+
+    ssh_authorized_key { $name:
       ensure => present,
       user => $user,
       key => $key,
@@ -16,4 +25,5 @@ class ssh {
       type => $key_type,
     }
   }
+
 }
