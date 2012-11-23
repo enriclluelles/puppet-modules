b8d3a5866d20d024203e36e32d841dd034acddfc
Francesc Esplugas <francesc.esplugas@gmail.com>
2012-11-23 17:40:07 +0100

    Fix syntax error

--- heroku/manifests/init.pp
+++ heroku/manifests/init.pp
@@ -1,6 +1,6 @@
 class heroku {
 
-  exec { 'heroku-toolbelt': }
+  exec { 'heroku-toolbelt':
     command => 'wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh',
     unless => 'test -f /etc/apt/sources.list.d/heroku.list',
     user => root,
